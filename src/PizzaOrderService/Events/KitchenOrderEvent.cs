519f6d465bac3bda00030eb53bef06e778f5decb
Fernando Ventura <ferventurart@gmail.com>
2024-06-27 05:46:44 -0400

    ✏️ Renaming event
    Renaming to KitchenOrderEvent

---
+++ src/PizzaOrderService/Events/KitchenOrderEvent.cs
@@ -0,0 +1,16 @@
+using System.Text.Json.Serialization;
+using PizzaOrderService.Models;
+
+namespace PizzaOrderService.Events;
+
+public record KitchenOrderEvent
+{
+    public Guid Id { get; init; }
+    public int Quantity { get; init; }
+    public DateTime CreatedAt { get; init; }
+    [JsonConverter(typeof(JsonStringEnumConverter))]
+    public PizzaSize Size { get; init; }
+    public List<string> Toppings { get; init; } = [];
+    [JsonConverter(typeof(JsonStringEnumConverter))]
+    public OrderStatus Status { get; init; }
+}
